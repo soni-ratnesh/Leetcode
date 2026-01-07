2d0aa24847134f0703d39e5f8695a57b9552b5d8
Ratnesh Kumar <ratneshk.99@gmail.com>
2026-01-07 01:39:51 -0600

    Time: 11 ms (48.39%), Space: 30.8 MB (7.63%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,32 +1,17 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        result = 0
+        result = 10**6
 
-        curr_sum = nums[0]
-        ii, jj=0,1
-        while jj<len(nums) and curr_sum <target:
+        ii, jj = 0,0
+        curr_sum = 0
+        while jj<len(nums):
             curr_sum += nums[jj]
             jj+=1
-
-        jj-=1
-        result = (jj-ii+1) if jj<len(nums) and curr_sum >=target else 0
-   
-        while jj<len(nums):
-
-            if result ==1:
-                return result
-            if curr_sum >= target:
-
-                result = min(result, jj-ii+1)
-                curr_sum -= nums[ii]
+            while curr_sum>=target:
+                result = min(result, jj-ii)
+                curr_sum-=nums[ii]
                 ii+=1
-                
-            else:
-                jj+=1
-                if jj>=len(nums):
-                    break
-                curr_sum += nums[jj]
-                
+           
+            
 
-
-        return result
+        return result if result!=10**6 else 0            
