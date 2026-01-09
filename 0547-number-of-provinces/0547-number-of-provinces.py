1166fafe0691517072496e26b11c3073303382f3
Ratnesh Kumar <ratneshk.99@gmail.com>
2026-01-08 19:59:07 -0600

    Time: 93 ms (5.56%), Space: 20.5 MB (5.88%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        def find(x):
+            if root[x] == x:
+                return x
+            root[x] = find(root[x])
+            return root[x]
+
+        def union(root, x, y):
+            root_x, root_y = find(x), find(y)
+            for ii in range(len(root)):
+                if root[ii] == root[root_y]:
+                    root[ii] = root[root_x]
+        
+        
+        root = [i for i in range(len(isConnected))]
+            
+        for ii in range(len(isConnected)):
+            for jj in range(len(isConnected)):
+                if isConnected[ii][jj] == 1:
+                    union(root, ii,jj)
+        for ii in range(len(root)):
+            find(ii)
+        return len(set(root))
+                    
+        
+        
