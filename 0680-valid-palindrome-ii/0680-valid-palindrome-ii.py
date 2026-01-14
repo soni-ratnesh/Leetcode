7bb8d7379d01482d831f7f26123e6987229696b6
Ratnesh Kumar <ratneshk.99@gmail.com>
2026-01-13 23:03:14 -0600

    Time: 159 ms (5.09%), Space: 28.4 MB (5.04%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def check(st, ed, k):
+            if ed-st<1:
+                return True
+            elif s[st] == s[ed]:
+                return check(st+1, ed-1, k)
+            elif k:
+                return check(st, ed-1, k-1) or check(st+1, ed, k-1)
+            else:
+                return False
+        if len(s)<3:
+            return True
+        return check(0, len(s)-1, 1)
